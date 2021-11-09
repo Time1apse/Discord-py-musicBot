24b321f0bba066e2d6d58407c058782b7309b722
Timelapse <d.elisey@yandex.ru>
2021-11-09 16:20:03 +0300

    fix some bugs

--- Paparser.py
+++ Paparser.py
@@ -21,7 +21,6 @@ def music(name: str):
 
 def playlist(url: str, x=0):
    playlist = Playlist(url)
-<<<<<<< HEAD
    playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
    ids = ""
    for url in playlist.video_urls:
@@ -31,16 +30,6 @@ def playlist(url: str, x=0):
    print(ids)
    return(ids)
    
-=======
-   if(x==1):
-        playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
-        ids = ""
-        for url in playlist.video_urls:
-            id = url
-            ids += id+" "
-        ids = ids.split( )
-        return(ids)
->>>>>>> 34aa7979e1021849b4164e7f2beee565c3404d20
 
 if __name__ == "__main__":
    playlist("https://www.youtube.com/playlist?list=PLADmR6fAuPwfZK6Su-O4p3Qyfm-dliwW1", 1)
