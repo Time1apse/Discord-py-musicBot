fbb7b6d6c6e8a67bdfde7d34cebd6822d06d6f20
Timelapse <d.elisey@yandex.ru>
2022-12-21 01:07:39 +0300

    Updated to new version of discord library

--- Amadeus_Bot.py
+++ Amadeus_Bot.py
@@ -1,19 +1,31 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from config import settings
 from music import Player
+import asyncio
+import discord
+from discord.ext import commands
+from discord.ext.commands.core import check
+import youtube_dl
+import pafyfixed as pafy
+from Paparser import music, playlist
+import asyncio
+from discord import FFmpegPCMAudio, PCMVolumeTransformer
 
 activity = discord.Activity(type=discord.ActivityType.listening, name="$help")
 
-bot = commands.Bot(command_prefix="$", activity=activity, status=discord.Status.do_not_disturb)
+bot = commands.Bot(command_prefix="$", activity=activity, status=discord.Status.do_not_disturb, intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("Bruh")
+    bot.loop.create_task(setup())
+
 
 async def setup():
     await bot.wait_until_ready()
-    bot.add_cog(Player(bot))
+    await bot.add_cog(Player(bot))
 
-bot.loop.create_task(setup())
+
 bot.run(settings["token"])
+
